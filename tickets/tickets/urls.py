f6d41cae430e5abef92ec75b043bc01543b4dfe9
Ariel Plaza <arielplazasalinas@gmail.com>
2025-03-21 16:08:39 -0300

    Se agrega reports

--- tickets/tickets/urls.py
+++ tickets/tickets/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tickets/', include('gestion_tk.urls'))
+    path('tickets/', include('gestion_tk.urls')),
+    path('reports/', include('reports.urls')),
 ]
